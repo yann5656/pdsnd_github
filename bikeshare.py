fb36bd79cfbeebc537c8dc14fda9a32931b2f4ab
yann <yann.michel@daimler.com>
2020-11-01 18:38:32 +0100

    added comment for the data files

--- bikeshare.py
+++ bikeshare.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import calendar
 
+#used bikeshare data files
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
